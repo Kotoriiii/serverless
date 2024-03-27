b77299aff894c9275d20a6ed53b018aa5a7a612c
JasonLiii <ljq19970216@gmail.com>
2024-03-27 12:36:03 -0400

    udpate the file

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const functions = require("@google-cloud/functions-framework");
 const mailgun = require("mailgun-js");
+const mysql = require("mysql2");
 require("dotenv").config();
 
 functions.cloudEvent("sendVerificationEmail", (cloudEvent) => {
   const base64name = cloudEvent.data.message.data;
-  const message = JSON.parse(Buffer.from(base64name, "base64").toString());
-  const userEmail = message.email;
-  const userId = message.id;
-  const verificationCode = message.verificationCode;
-  console.log(userEmail, userId, verificationCode);
+  const messageData = JSON.parse(Buffer.from(base64name, "base64").toString());
+  const userEmail = messageData.data.email;
+  const userId = messageData.data.id;
+  const verifyCode = messageData.data.verifyCode;
+  console.log(userEmail, userId, verifyCode);
 
   const DOMAIN = "jasonlidevelop.me";
   const mg = mailgun({ apiKey: process.env["API_KEY"], domain: DOMAIN });
 
-  const verificationLink = `http://api.development.com/verify?user=${userId}&code=${verificationCode}`;
+  const verificationLink = `http://api.development.com/verify?id=${userId}&code=${verifyCode}`;
 
   const data = {
     from: `Excited User <mailgun@${DOMAIN}>`,
@@ -29,15 +30,28 @@ functions.cloudEvent("sendVerificationEmail", (cloudEvent) => {
     console.log(body);
   });
 
-  // Your logic to send the email goes here
   console.log(`Sending verification email to ${userEmail}`);
-});
 
-// sendVerificationEmail({
-//   data: Buffer.from(
-//     JSON.stringify({
-//       id: "1",
-//       email: "ljq19970216@gmail.com",
-//     })
-//   ).toString("base64"),
-// });
+  const pool = mysql.createPool(process.env["DATABASE_URL"]);
+
+  pool.getConnection(function (err, connection) {
+    if (err instanceof Error) {
+      console.log(err);
+      return;
+    }
+
+    const sql =
+      "UPDATE `User` SET `isSend` = true WHERE `id` = ? and `verifyCode` = ? ";
+    pool.execute(sql, [userId, verifyCode], function (err, result, fields) {
+      if (err instanceof Error) {
+        console.log(err);
+        return;
+      }
+
+      console.log(result);
+      console.log(fields);
+    });
+
+    connection.release();
+  });
+});
